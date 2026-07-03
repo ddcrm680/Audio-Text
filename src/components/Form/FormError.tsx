2d9c113787d4c74902b5e2ae6ef8d44ad68005c7
Sakshi Tiwari <sakshi.tiwari@detailingdevils.in>
2026-07-03 16:42:09 +0530

    update css styling

--- src/components/Form/FormError.tsx
+++ src/components/Form/FormError.tsx
@@ -11,5 +11,5 @@ export default function FormError({ name }: FormErrorProps) {
 
   if (!meta.touched && submitCount === 0) return null;
 
-  return <p className="mt-1 text-sm text-red-500">{meta.error}</p>;
+  return <p className="mt-[15px] text-sm text-red-500">{meta.error}</p>;
 }
