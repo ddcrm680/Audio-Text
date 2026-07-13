eb0ec233b9e5eaff26bddeec36d3b57d64ede9d0
Sakshi Tiwari <sakshi.tiwari@detailingdevils.in>
2026-07-13 17:16:14 +0530

    done the blog detail page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -67,142 +67,142 @@ export default async function BlogDetails({ params }: Props) {
               <div>
                 <div className="flex flex-wrap items-center justify-between pb-4 dark:border-white/10">
                   <div className="flex flex-wrap items-center">
-                    <div className="mb-10">
+                    <div className="overflow-hidden rounded-[6px] border border-[#d8e0e8] bg-white shadow-sm dark:border-gray-700 dark:bg-[#111827]">
+                      {/* Featured Image */}
                       <Image
                         src={blog.image}
                         alt={blog.title}
                         width={1100}
                         height={600}
-                        className="mb-10 w-full"
+                        className="w-full rounded-[6px] border border-[#d8e0e8] object-cover"
                       />
 
-                      <div
-                        className="prose prose-lg dark:prose-invert max-w-none [&_a]:text-[#48AFDB] [&_a]:no-underline hover:[&_a]:underline [&_blockquote]:border-l-4 [&_blockquote]:border-[#48AFDB] [&_blockquote]:pl-5 [&_blockquote]:italic [&_h1]:mb-6 [&_h1]:text-[52px] [&_h1]:leading-[60px] [&_h1]:font-extralight [&_h1]:text-[#48AFDB] [&_h1]:uppercase [&_h2]:mb-6 [&_h2]:text-[44px] [&_h2]:leading-[52px] [&_h2]:font-extralight [&_h2]:text-[#48AFDB] [&_h2]:uppercase [&_h3]:mb-5 [&_h3]:text-[34px] [&_h3]:leading-[42px] [&_h3]:font-light [&_h3]:text-[#48AFDB] [&_h4]:mb-4 [&_h4]:text-[28px] [&_h4]:leading-[36px] [&_h4]:font-light [&_h4]:text-[#48AFDB] [&_h5]:mb-3 [&_h5]:text-[24px] [&_h5]:font-light [&_h5]:text-[#48AFDB] [&_h6]:mb-3 [&_h6]:text-[20px] [&_h6]:font-medium [&_h6]:text-[#48AFDB] [&_img]:my-8 [&_img]:rounded-lg [&_li]:mb-2 [&_ol]:my-6 [&_ol]:list-decimal [&_ol]:pl-8 [&_p]:mb-5 [&_strong]:font-semibold [&_table]:w-full [&_table]:border-collapse [&_ul]:my-6 [&_ul]:list-disc [&_ul]:pl-8"
-                        dangerouslySetInnerHTML={{ __html: blog.desc }}
-                      />
-                    </div>
+                      {/* Blog Content */}
+                      <div className="p-10">
+                        <div
+                          className="prose max-w-none text-[#555] [&_a]:text-[#48AFDB] [&_a]:no-underline hover:[&_a]:underline [&_h1]:mb-6 [&_h1]:text-[34px] [&_h1]:font-light [&_h1]:text-[#48AFDB] [&_h1]:uppercase [&_h2]:mb-5 [&_h2]:text-[30px] [&_h2]:font-light [&_h2]:text-[#48AFDB] [&_h2]:uppercase [&_h3]:mb-4 [&_h3]:text-[24px] [&_h3]:font-normal [&_h3]:text-[#48AFDB] [&_img]:rounded-md [&_ol]:list-decimal [&_ol]:pl-6 [&_p]:mb-5 [&_p]:text-[17px] [&_p]:leading-[34px] [&_ul]:list-disc [&_ul]:pl-6"
+                          dangerouslySetInnerHTML={{ __html: blog.desc }}
+                        />
 
-                    {/* Author */}
-                    <div className="mt-[53px] w-full bg-[#f4f7fa] px-[35px] py-[30px] dark:bg-[#1f2937]">
-                      <div className="flex items-start gap-6">
-                        <div className="flex h-[94px] w-[94px] items-center justify-center rounded-full bg-white">
-                          <Image
-                            src="/images/blog/user.jpg"
-                            className="rounded-full"
-                            width={72}
-                            height={72}
-                            alt=""
-                          />
-                        </div>
-
-                        <div>
-                          <h3 className="text-[24px] font-light text-black dark:text-white">
-                            {blog?.author}
-                          </h3>
-
-                          <p className="mt-[3px] flex flex-wrap items-center text-[15px] text-[#75889C] dark:text-gray-400">
-                            <span>
-                              {formatBlogDate(blog.publish_at)} in&nbsp;
-                            </span>
-
-                            {blog.category.map((item, index) => (
-                              <span
-                                key={item.slug}
-                                className="flex items-center"
-                              >
-                                <Link
-                                  href={`/blog/category/${item.slug}`}
-                                  className="text-[#48AFDB] hover:underline"
-                                >
-                                  {item.name}
-                                </Link>
-
-                                {index < blog.category.length - 1 && (
-                                  <span className="mx-1 text-[#75889C] dark:text-gray-400">
-                                    ,
-                                  </span>
-                                )}
-                              </span>
-                            ))}
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="mt-[52px] mb-[23px] flex w-full flex-col gap-[18px] border-b border-[#d6dde3] pb-[10px] dark:border-gray-700">
-                      {/* Share Icons */}
-                      <div className="flex h-[35px] items-center gap-4">
-                        {socials.map((item, index) => (
-                          <a
-                            key={index}
-                            href={item.link}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className={`dark:hover:bg-primary-blue hover:bg-primary-blue flex h-[35px] w-[35px] items-center justify-center rounded-full bg-gray-300 transition-colors duration-200 dark:bg-[#2d3748]`}
-                          >
-                            <Image
-                              src={item.icon}
-                              alt=""
-                              width={20}
-                              height={20}
-                            />
-                          </a>
-                        ))}
-                      </div>
-                      <div className="flex w-full flex-wrap justify-end text-[15px] text-[#75889C]">
-                        <span>{formatBlogDate(blog.publish_at)}</span>
-
-                        {blog.tag.map((tag) => (
-                          <span key={tag.slug} className="flex items-center">
-                            <span className="mx-[10px] text-[#75889C]">|</span>
-
-                            <Link
-                              href={`/blog/tag/${tag.slug}`}
-                              className="text-[#48AFDB] hover:underline"
-                            >
-                              {tag.name}
-                            </Link>
-                          </span>
-                        ))}
-                      </div>
-                    </div>
-                    <div className="w-full pb-[52px]">
-                      <div className="flex items-center justify-between">
-                        {/* Previous */}
-
-                        {prevBlog ? (
-                          <Link
-                            href={`/blog/${prevBlog.slug}`}
-                            className="group flex items-center gap-2"
-                          >
-                            <div className="flex h-[34px] w-[34px] items-center justify-center rounded-full bg-[#48AFDB] text-white transition group-hover:bg-[#2d9fd5]">
-                              <ChevronLeft size={18} />
+                        {/* Author */}
+                        <div className="mt-6 rounded-lg border border-[#bec8cf] bg-[#eff4ff] p-3">
+                          <div className="flex items-center gap-4">
+                            <div className="flex h-12 w-12 items-center justify-center rounded-full bg-[#d8edf8]">
+                              <Image
+                                src="/images/blog/user.jpg"
+                                alt=""
+                                width={30}
+                                height={30}
+                                className="rounded-full"
+                              />
                             </div>
 
                             <div>
-                              <p className="text-[15px] text-[#444] uppercase dark:text-gray-300">
-                                Previous Post
+                              <h4 className="text-[18px] leading-[20px] font-semibold text-[#222]">
+                                {blog.author}
+                              </h4>
+
+                              <p className="mt-1 flex flex-wrap text-[14px] leading-[20px] text-[#666]">
+                                <span>
+                                  {formatBlogDate(blog.publish_at)} in&nbsp;
+                                </span>
+
+                                {blog.category.map((item, index) => (
+                                  <span key={item.slug}>
+                                    <Link
+                                      href={`/blog/category/${item.slug}`}
+                                      className="font-semibold text-[#0077a8] hover:underline"
+                                    >
+                                      {item.name}
+                                    </Link>
+
+                                    {index !== blog.category.length - 1 && (
+                                      <span className="mx-1">,</span>
+                                    )}
+                                  </span>
+                                ))}
                               </p>
                             </div>
+                          </div>
+                        </div>
+
+                        {/* Footer */}
+                        <div className="mt-6 border-t border-[#bec8cf] pt-4">
+                          <div className="flex flex-col justify-between gap-4">
+                            <div className="flex gap-2">
+                              {socials.map((item, index) => (
+                                <a
+                                  key={index}
+                                  href={item.link}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                  className="bg-primary-blue flex h-8 w-8 items-center justify-center rounded-full border border-[#d8e1e8] transition-all duration-200 hover:border-[#48AFDB] hover:bg-[#39a6d6] dark:border-gray-600 dark:bg-[#2d3748] dark:hover:bg-[#48AFDB]"
+                                >
+                                  <Image
+                                    src={item.icon}
+                                    alt=""
+                                    width={14}
+                                    height={14}
+                                    className="opacity-80 transition-opacity duration-200 hover:opacity-100"
+                                  />
+                                </a>
+                              ))}
+                            </div>
+
+                            <div className="flex flex-wrap items-center text-[13px] text-[#777]">
+                              <span>{formatBlogDate(blog.publish_at)}</span>
+
+                              {blog.tag.map((tag) => (
+                                <span
+                                  key={tag.slug}
+                                  className="flex items-center"
+                                >
+                                  <span className="mx-2">|</span>
+
+                                  <Link
+                                    href={`/blog/tag/${tag.slug}`}
+                                    className="text-[#0077a8] hover:underline"
+                                  >
+                                    {tag.name}
+                                  </Link>
+                                </span>
+                              ))}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="mt-12 w-full border-y border-[#d9e3ea] py-6">
+                      <div className="flex items-center justify-between">
+                        {prevBlog ? (
+                          <Link
+                            href={`/blog/${prevBlog.slug}`}
+                            className="group flex items-center gap-3"
+                          >
+                            <div className="flex h-6 w-6 items-center justify-center rounded-full bg-[#0077a8] text-white transition-colors duration-200 group-hover:bg-[#005f87]">
+                              <ChevronLeft size={16} strokeWidth={2.5} />
+                            </div>
+
+                            <span className="text-[16px] font-normal tracking-[0.5px] text-[#0077a8] uppercase">
+                              Previous Post
+                            </span>
                           </Link>
                         ) : (
                           <div />
                         )}
-
-                        {/* Next */}
 
                         {nextBlog ? (
                           <Link
                             href={`/blog/${nextBlog.slug}`}
-                            className="group flex items-center gap-2"
+                            className="group flex items-center gap-3"
                           >
-                            <div className="text-right">
-                              <p className="text-[15px] text-[#444] uppercase dark:text-gray-300">
-                                Next Post
-                              </p>
-                            </div>
+                            <span className="text-[16px] font-normal tracking-[0.5px] text-[#0077a8] uppercase">
+                              Next Post
+                            </span>
 
-                            <div className="flex h-[34px] w-[34px] items-center justify-center rounded-full bg-[#48AFDB] text-white transition group-hover:bg-[#2d9fd5]">
-                              <ChevronRight size={18} />
+                            <div className="flex h-6 w-6 items-center justify-center rounded-full bg-[#0077a8] text-white transition-colors duration-200 group-hover:bg-[#005f87]">
+                              <ChevronRight size={16} strokeWidth={2.5} />
                             </div>
                           </Link>
                         ) : (
@@ -210,20 +210,16 @@ export default async function BlogDetails({ params }: Props) {
                         )}
                       </div>
                     </div>
-
-                    <div className="mb-[14px]">
-                      <h2 className="mb-[22px] text-[28px] leading-[38px] font-extralight text-[#48AFDB] uppercase">
+                    <div className="mt-12 w-full">
+                      <h2 className="border-primary-blue mb-6 border-l-4 pl-3 text-[20px] leading-[28px] font-semibold text-[#0b3156] dark:text-sky-400">
                         Related Posts
                       </h2>
-                      <div className="flex gap-[18px]">
+
+                      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
                         {relatedPosts.slice(0, 3).map((blog, index, arr) => (
                           <div
                             key={blog.id}
-                            className={` ${index !== 0 ? "pt-[34px]" : ""} ${
-                              index === arr.length - 1
-                                ? "border-b border-[#d9d9d9] pb-[35px] lg:border-0 lg:pb-0 dark:border-gray-700"
-                                : ""
-                            } `}
+                            className={` ${index !== 0 ? "pt-[34px]" : ""} hover:border-primary-blue rounded-lg border border-[#bec8cf] bg-[#eff4ff] p-[8px]`}
                           >
                             <RelatedPost
                               title={blog.title}
